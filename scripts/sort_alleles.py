e900b875b4c63b74bee24023d2b1d643cdf608e5
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2024-04-15 10:26:54 +0200

    Add argument parser, and write output to standard out

--- scripts/sort_alleles.py
+++ scripts/sort_alleles.py
@@ -1,36 +1,39 @@
+#!/usr/bin/env python3
+
+import argparse
 import pandas as pd
+import functools
 import sys
 
-#Usage: sort_alleles.py path/to/input.csv genes(e.g. A,B,C)
-
-#check that a path has been provided as an argument in the command line and genes have been specified
-assert len(sys.argv) > 2, 'Provide a path to the input csv file and genes to be sorted (e.g. A,B,C)' 
-
-input_file = sys.argv[1] #use command line argument as the input csv path
-
-df = pd.read_csv(input_file)
-
-#create list of genes that need to be sorted (specified in command prompt)
-genes = sys.argv[2]
-if genes == 'all': #for ease of use
-    genes_list = ['A','B','C','DRB1','DRB3','DRB4','DRB5','DQA1','DQB1','DPB1']
-else: genes_list = genes.split(',')
-hla_genes = []
-for gene in genes_list:
-    hla_genes.append(f'HLA-{gene}')
-
-print(f'Genes that are being sorted: {", ".join(hla_genes)}')
-
-#function to sort pairs of alleles
-def sort_allele_pair(row):
+def sort_allele_pair(hla_genes, row):
+    """Sort pairs of alleles"""
     for gene in hla_genes:
         alleles = [f'{gene}', f'{gene} (2)']
         row[alleles] = sorted(row[alleles])
     return row
 
-#apply the sorting function to each row (axis=1 > apply row-wise)
-df = df.apply(sort_allele_pair, axis=1)
+def main (input_file, genes):
+    df = pd.read_csv(input_file)
 
-#write the sorted df to an output csv file
-output_file = input_file.replace('.csv', '_sorted.csv')
-df.to_csv(output_file, index=False)
+    hla_genes = []
+    for gene in genes:
+        hla_genes.append(f'HLA-{gene}')
+
+    print(f'Genes that are being sorted: {", ".join(hla_genes)}')
+
+    # Make a temporary function that has hla_genes set
+    sorter = functools.partial(sort_allele_pair, hla_genes)
+    #apply the sorting function to each row (axis=1 > apply row-wise)
+    df = df.apply(sorter, axis=1)
+
+    #write the sorted df to an output csv file
+    df.to_csv(sys.stdout, index=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--input", required=True, help="Input CSV file to be sorted")
+    parser.add_argument("--genes", nargs='+', default=['A','B','C','DRB1','DRB3','DRB4','DRB5','DQA1','DQB1','DPB1'], help="Genes to include")
+
+    args = parser.parse_args()
+
+    main(args.input, args.genes)
